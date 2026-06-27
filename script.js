00d78d1b452017204c149ea416cff53d9ec91545
nonameBGIST <foodi00048@gmail.com>
2026-06-28 02:22:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 
+const burgerBtn = document.getElementById("burger-btn");
+const nav = document.querySelector("nav");
 const navLinks = document.querySelectorAll("nav a");
+
+
+burgerBtn.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
 
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
-
         navLinks.forEach(l => l.classList.remove("active"));
-
         link.classList.add("active");
+        
+        
+        nav.classList.remove("open");
     });
 });
