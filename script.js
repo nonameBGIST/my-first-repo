9c39daade62ae25877c9d64918af695337a0b1fa
nonameBGIST <foodi00048@gmail.com>
2026-06-28 01:34:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("Привет! Скрипт подключен и работает.");
+
+
+const dateSpan = document.getElementById("update-date");
+const today = new Date();
+dateSpan.textContent = today.toLocaleDateString("ru-RU");
