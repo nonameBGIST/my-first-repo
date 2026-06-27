c3a26f9dd11b01b610daddd29768f6d1521eb04e
nonameBGIST <foodi00048@gmail.com>
2026-06-28 02:01:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
-console.log("Привет! Скрипт подключен и работает.");
+const navLinks = document.querySelectorAll("nav a");
 
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
 
-const dateSpan = document.getElementById("update-date");
-const today = new Date();
-dateSpan.textContent = today.toLocaleDateString("ru-RU");
+        navLinks.forEach(l => l.classList.remove("active"));
+
+        link.classList.add("active");
+    });
+});
